0c6321dcfb4ba38161dda8a70c1dbdbb89f95559
anjing <anjingcuc@gmail.com>
2019-10-30 15:14:42 +0800

    尝试完善脚本。

--- mklinks.ps1
+++ mklinks.ps1
@@ -1,7 +1,7 @@
 Get-ChildItem -Path .\docs\*\* -Recurse -Directory -Force -ErrorAction SilentlyContinue |
     ForEach-Object {
         $link = Join-Path -Path $_.FullName -ChildPath "slideshow.html"
-        $target = "..\..\..\material\slideshow.html"
+        $target = Resolve-Path -Path ".\material\slideshow.html" -Relative
 
         New-Item -Path $link -Force -ItemType SymbolicLink -Value $target
     }
